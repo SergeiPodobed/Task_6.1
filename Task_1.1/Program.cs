d51bd3a151c03e1e0a0800c28c17b2e4ad1f9883
SergeiPodobed <izotep19@yandex.ru>
2020-03-22 13:04:10 +0300

    Add project files.

---
+++ Task_1.1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Task_1._1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] mas1 = new int[4];
+            int[] mas2 = new int[4];
+            int[] mas3 = new int[4];
+
+            Random rnd = new Random();
+
+            Console.WriteLine($"Элементы массива mas1");
+                        
+                for (int i = 0; i < 4; i++)
+            {
+                mas1[i] = rnd.Next(100);
+
+                Console.WriteLine ($" {mas1[i]} ");
+
+            }
+
+            Console.WriteLine($"Введите 4 элемента массива mas2 от 0 до 100");
+            {
+                for (int i = 0; i < 4; i++)
+
+                    mas2[i] = int.Parse(Console.ReadLine());
+            }
+
+
+            Console.WriteLine($"Поэлементная сумма массивов mas1 и mas2");
+
+            {
+
+                for (int i = 0; i < 4; i++)
+
+                    mas3[i] = mas1[i] + mas2[i];
+
+                foreach (int i in mas3)
+
+                    Console.WriteLine(i);
+            }
+
+            
+
+
+            
+        }
+    }
+}
